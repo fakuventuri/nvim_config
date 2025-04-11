be021a77319424f7c4b37e641a30fa23932156bf
Ivan Brocas <104206321+IvanB101@users.noreply.github.com>
2025-04-11 14:33:51 +0200

    java configuration working

--- lua/plugins/configs/nvim_dap.lua
+++ lua/plugins/configs/nvim_dap.lua
@@ -4,3 +4,14 @@ vim.keymap.set("n", "<leader>db", function()
     local sidebar = widgets.sidebar(widgets.scopes)
     sidebar.open()
 end, { desc = "open debugging sidebar" })
+
+local dap = require("dap")
+dap.configurations.java = {
+    {
+        type = "java",
+        request = "attach",
+        name = "Debug (Attach) - Remote",
+        hostName = "127.0.0.1",
+        port = 5005,
+    },
+}
