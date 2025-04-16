a4e4411956f2dbff2f02fa68e92ed8e6de912821
Ivan Brocas <104206321+IvanB101@users.noreply.github.com>
2025-04-16 21:45:11 +0200

    changed some remaps and added others and cmd for fugitive
    - Extracted `vim.keymap.set` to a local function
    - Added some user mostly to open the result of some fugitive commands in
      a new tab by default
    - Changed keymap related to tab opening and closing due to frequence of
      use

--- lua/plugins/configs/fugitive.lua
+++ lua/plugins/configs/fugitive.lua
@@ -1,1 +1,18 @@
-vim.keymap.set("n", "<leader>gs", "<cmd>tab Git<cr>", { desc = "" })
+local remap = vim.keymap.set
+local user_cmd = vim.api.nvim_create_user_command
+
+remap("n", "<leader>gs", "<cmd>tab Git<cr>", { desc = "Git status" })
+remap("n", "<leader>gv", function()
+    local commit = vim.fn.getreg('"')
+    vim.cmd("Gdiff " .. commit)
+end, { desc = "Git vertical diffsplit (with commit on register \")" })
+
+user_cmd("Gdiff", function(params)
+    vim.cmd("tab Gvdiffsplit " .. (params.args or ""))
+end, { desc = "Git log on other tab", nargs = "?" })
+
+user_cmd("Glog", function(params)
+    vim.cmd("tab Git log " .. (params.args or ""))
+end, { desc = "Git log on other tab", nargs = "?" })
+user_cmd("Ggraph", "Glog --all --decorate --oneline --graph", { desc = "Git graph" })
+user_cmd("Ggraphv", "Glog --all --decorate --graph", { desc = "Git graph verbose" })
--- lua/remap.lua
+++ lua/remap.lua
@@ -1,72 +1,68 @@
--- vim.keymap.set("n", "", "", { desc = "" })
-
+local remap = vim.keymap.set
 local quickfix = require("utils.quickfix")
 -- BASE
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "open netrw" })
+remap("n", "<leader>e", vim.cmd.Ex, { desc = "open netrw" })
 
-vim.keymap.set("n", "<tab>", "gt", { desc = "go to next tab" })
-vim.keymap.set("n", "<s-tab>", "gT", { desc = "go to previos tab" })
+remap("n", "<tab>", "gt", { desc = "go to next tab" })
+remap("n", "<s-tab>", "gT", { desc = "go to previos tab" })
 
-vim.keymap.set({"n", "v"}, "L", "$", { desc = "go to line end" })
-vim.keymap.set("n", "dL", "d$", { desc = "go to line end" })
-vim.keymap.set({"n", "v"}, "H", "^", { desc = "delete till line start" })
-vim.keymap.set("n", "dH", "d^", { desc = "delete till line start" })
+remap({ "n", "v" }, "L", "$", { desc = "go to line end" })
+remap("n", "dL", "d$", { desc = "go to line end" })
+remap({ "n", "v" }, "H", "^", { desc = "delete till line start" })
+remap("n", "dH", "d^", { desc = "delete till line start" })
+remap("n", "D", vim.diagnostic.open_float, { desc = "delete till line start" })
 
-vim.keymap.set("n", "D", vim.diagnostic.open_float, { desc = "delete till line start" })
+remap("n", "<leader>rp", ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "replace ocurrencies" })
+remap("v", "<leader>sr", "y/<C-r>\"", { desc = "search marked text ocurrencies" })
+remap("v", "K", ":m '>+1<CR>gv=gv", { desc = "move marked text up" })
+remap("v", "J", ":m '<-2<CR>gv=gv", { desc = "move marked text down" })
+remap("n", "J", "mzJ`z", { desc = "J maintains cursor position" })
+remap("n", "<C-d>", "<C-d>zz", { desc = "centeres <C-d>" })
+remap("n", "<C-u>", "<C-u>zz", { desc = "centeres <C-u>" })
+remap("n", "n", "nzzzv", { desc = "Centered cursor n" })
+remap("n", "N", "Nzzzv", { desc = "Centered cursor N" })
 
-vim.keymap.set("n", "<leader>rp", ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "replace ocurrencies" })
-vim.keymap.set("v", "<leader>sr", "y/<C-r>\"", { desc = "search marked text ocurrencies" })
-vim.keymap.set("v", "K", ":m '>+1<CR>gv=gv", { desc = "move marked text up" })
-vim.keymap.set("v", "J", ":m '<-2<CR>gv=gv", { desc = "move marked text down" })
-vim.keymap.set("n", "J", "mzJ`z", { desc = "J maintains cursor position" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "centeres <C-d>" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "centeres <C-u>" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Centered cursor n" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Centered cursor N" })
+remap({ "n", "v" }, "<leader>y", "\"+y", { desc = "yank to clipboard" })
+remap({ "n", "v" }, "<leader>p", "\"+p", { desc = "paste from system clipboard" })
+remap({ "n", "v" }, "<leader>P", "\"+p", { desc = "paste from system clipboard" })
+remap("v", "r", "\"_dp", { desc = "replace marked with clipboard" })
+remap("v", "<leader>r", "\"_d\"+p", { desc = "replace marked with system clipboard" })
 
-vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y", { desc = "yank to clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>p", "\"+p", { desc = "paste from system clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>P", "\"+p", { desc = "paste from system clipboard" })
-vim.keymap.set("v", "r", "\"_dp", { desc = "replace marked with clipboard" })
-vim.keymap.set("v", "<leader>r", "\"_d\"+p", { desc = "replace marked with system clipboard" })
+remap("n", "U", "<C-r>", { desc = "redo" })
+remap("n", "<C-z>", "<nop>", { desc = "nothing" })
 
-vim.keymap.set("n", "U", "<C-r>", { desc = "redo" })
-vim.keymap.set("n", "<C-z>", "<nop>", { desc = "nothing" })
+remap("i", "<C-j>", "<down>", { desc = "down in insert mode" })
+remap("i", "<C-k>", "<up>", { desc = "up in insert mode" })
+remap("i", "<C-h>", "<left>", { desc = "left in insert mode" })
+remap("i", "<C-l>", "<right>", { desc = "right in insert mode" })
 
-vim.keymap.set("i", "<C-j>", "<down>", { desc = "down in insert mode" })
-vim.keymap.set("i", "<C-k>", "<up>", { desc = "up in insert mode" })
-vim.keymap.set("i", "<C-h>", "<left>", { desc = "left in insert mode" })
-vim.keymap.set("i", "<C-l>", "<right>", { desc = "right in insert mode" })
-
-vim.keymap.set("n", "Q", function() quickfix.toggle() end, { desc = "toggle quickfix list" })
-vim.keymap.set("n", "<leader>lo", function()
+remap("n", "Q", function() quickfix.toggle() end, { desc = "toggle quickfix list" })
+remap("n", "<leader>lo", function()
     vim.cmd('e' .. vim.lsp.get_log_path())
 end, { desc = "show error log" })
 
-vim.keymap.set("n", "<leader>ts", "<cmd>tab split<cr>", { desc = "tab split" })
-vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "new tab" })
+remap("n", "<C-m>", "<cmd>tab split<cr>", { desc = "tab split" })
+remap("n", "<C-n>", "<cmd>tabnew<cr>", { desc = "new tab" })
+remap("n", "Q", "<cmd>tabclose<cr>", { desc = "close tab" })
 
 -- TROUBLE
-vim.keymap.set("n", "<leader>sd", "<cmd>Trouble diagnostics toggle<cr>", { desc = "show diagnostics" })
-vim.keymap.set("n", "<leader>sb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+remap("n", "<leader>sd", "<cmd>Trouble diagnostics toggle<cr>", { desc = "show diagnostics" })
+remap("n", "<leader>sb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
     { desc = "show buffer diagnostics" })
-vim.keymap.set("n", "<leader>ss", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "show symbols" })
+remap("n", "<leader>ss", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "show symbols" })
 
 -- TELESCOPE
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find files" })
-vim.keymap.set("n", "<leader>fp", function()
+remap("n", "<leader>ff", builtin.find_files, { desc = "find files" })
+remap("n", "<leader>fp", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = "project grep" })
-vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "find git files" })
-vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "find git files" })
-vim.keymap.set("n", "<leader>fs", builtin.lsp_workspace_symbols, { desc = "find workspace symbols" })
-vim.keymap.set("n", "<leader>fbs", builtin.lsp_document_symbols, { desc = "find document symbols" })
-vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "telescope diagnostics" })
-vim.keymap.set("n", "<leader>fbd", function() builtin.diagnostics({ bufnr = 0 }) end,
+remap("n", "<leader>fg", builtin.git_files, { desc = "find git files" })
+remap("n", "<leader>fr", builtin.lsp_references, { desc = "find git files" })
+remap("n", "<leader>fs", builtin.lsp_workspace_symbols, { desc = "find workspace symbols" })
+remap("n", "<leader>fbs", builtin.lsp_document_symbols, { desc = "find document symbols" })
+remap("n", "<leader>fd", builtin.diagnostics, { desc = "telescope diagnostics" })
+remap("n", "<leader>fbd", function() builtin.diagnostics({ bufnr = 0 }) end,
     { desc = "telescope buffer diagnostics" })
-vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "key mappings" })
-
--- COMMENT
-vim.keymap.set("n", "<leader>c", require("Comment.api").toggle.linewise.current, { desc = "toggle comment" })
+remap("n", "<leader>fk", builtin.keymaps, { desc = "key mappings" })
