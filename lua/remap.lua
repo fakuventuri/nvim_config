531dbf96746da9fca4166927066e356bcd07e1ae
Ivan Brocas <104206321+IvanB101@users.noreply.github.com>
2025-04-25 09:42:53 +0200

    changed some mappings

--- lua/remap.lua
+++ lua/remap.lua
@@ -10,10 +10,12 @@ remap({ "n", "v" }, "L", "$", { desc = "go to line end" })
 remap("n", "dL", "d$", { desc = "go to line end" })
 remap({ "n", "v" }, "H", "^", { desc = "delete till line start" })
 remap("n", "dH", "d^", { desc = "delete till line start" })
-remap("n", "D", vim.diagnostic.open_float, { desc = "delete till line start" })
 
-remap("n", "<leader>rp", ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "replace ocurrencies" })
-remap("v", "<leader>sr", "y/<C-r>\"", { desc = "search marked text ocurrencies" })
+remap("n", "D", vim.diagnostic.open_float, { desc = "open float diagnostic" })
+
+remap("n", "<leader>rp", ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "replace all ocurrencies" })
+remap("v", "<leader>sr", "y/<C-r>\"<cr>", { desc = "search ocurrencies of marked text " })
+
 remap("v", "K", ":m '>+1<CR>gv=gv", { desc = "move marked text up" })
 remap("v", "J", ":m '<-2<CR>gv=gv", { desc = "move marked text down" })
 remap("n", "J", "mzJ`z", { desc = "J maintains cursor position" })
@@ -25,30 +27,35 @@ remap("n", "N", "Nzzzv", { desc = "Centered cursor N" })
 remap({ "n", "v" }, "<leader>y", "\"+y", { desc = "yank to clipboard" })
 remap({ "n", "v" }, "<leader>p", "\"+p", { desc = "paste from system clipboard" })
 remap({ "n", "v" }, "<leader>P", "\"+p", { desc = "paste from system clipboard" })
-remap("v", "r", "\"_dp", { desc = "replace marked with clipboard" })
-remap("v", "<leader>r", "\"_d\"+p", { desc = "replace marked with system clipboard" })
+remap("v", "r", "\"_dP", { desc = "replace marked with clipboard" })
+remap("v", "<leader>r", "\"_d\"+P", { desc = "replace marked with system clipboard" })
 
 remap("n", "U", "<C-r>", { desc = "redo" })
 remap("n", "<C-z>", "<nop>", { desc = "nothing" })
 
+remap("n", "<leader>q", function() quickfix.toggle() end, { desc = "toggle quickfix list" })
+remap("n", "<leader>lo", function()
+    vim.cmd('e' .. vim.lsp.get_log_path())
+end, { desc = "show error log" })
+
+remap("n", "<C-n>", "<cmd>tab split<cr>", { desc = "new tab" })
+remap("n", "Q", "<cmd>tabclose<cr>", { desc = "close tab" })
+-- insert mode
 remap("i", "<C-j>", "<down>", { desc = "down in insert mode" })
 remap("i", "<C-k>", "<up>", { desc = "up in insert mode" })
 remap("i", "<C-h>", "<left>", { desc = "left in insert mode" })
 remap("i", "<C-l>", "<right>", { desc = "right in insert mode" })
 
-remap("n", "Q", function() quickfix.toggle() end, { desc = "toggle quickfix list" })
-remap("n", "<leader>lo", function()
-    vim.cmd('e' .. vim.lsp.get_log_path())
-end, { desc = "show error log" })
-
-remap("n", "<C-m>", "<cmd>tab split<cr>", { desc = "tab split" })
-remap("n", "<C-n>", "<cmd>tabnew<cr>", { desc = "new tab" })
-remap("n", "Q", "<cmd>tabclose<cr>", { desc = "close tab" })
-
 -- TROUBLE
-remap("n", "<leader>sd", "<cmd>Trouble diagnostics toggle<cr>", { desc = "show diagnostics" })
-remap("n", "<leader>sb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+remap("n", "<leader>sd", "<cmd>Trouble diagnostics toggle focus=true<cr>", { desc = "show diagnostics" })
+remap("n", "<leader>se",
+    "<cmd>Trouble diagnostics toggle focus=true filter.severity=vim.diagnostic.severity.ERROR<cr>",
+    { desc = "show diagnostics" })
+remap("n", "<leader>sbd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
     { desc = "show buffer diagnostics" })
+remap("n", "<leader>sbe",
+    "<cmd>Trouble diagnostics toggle focus=true filter.severity=vim.diagnostic.severity.ERROR filter.buf=0<cr>",
+    { desc = "show diagnostics" })
 remap("n", "<leader>ss", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "show symbols" })
 
 -- TELESCOPE
