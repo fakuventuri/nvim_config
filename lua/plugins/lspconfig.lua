20fd25f0f20634f1ef7fcc1bfd0f76cf557b2cb9
Ivan Brocas <104206321+IvanB101@users.noreply.github.com>
2025-05-09 13:46:20 +0200

    removed signature help from lspconfig
    Since the information can be seen with hover, and it was giving error
    messages when editing git commits, it was removed.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,7 +2,7 @@
 -- This will avoid an annoying layout shift in the screen
 vim.opt.signcolumn = 'yes'
 vim.diagnostic.config({ virtual_text = true })
-local api = vim.api
+local remap = vim.keymap.set
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem = {
@@ -24,54 +24,20 @@ capabilities.textDocument.completion.completionItem = {
 }
 
 local function load_mappings(opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>ra', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'x' }, '<leader>fm', function() vim.lsp.buf.format({ async = true }) end, opts)
-    vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
-end
-
-local function check_triggeredChars(triggerChars)
-    local cur_line = api.nvim_get_current_line()
-    local pos = api.nvim_win_get_cursor(0)[2]
-    local prev_char = cur_line:sub(pos - 1, pos - 1)
-    local cur_char = cur_line:sub(pos, pos)
-
-    for _, char in ipairs(triggerChars) do
-        if cur_char == char or prev_char == char then
-            return true
-        end
-    end
-end
-
-local function setup(client, bufnr)
-    local group = api.nvim_create_augroup("LspSignature", { clear = false })
-    api.nvim_clear_autocmds { group = group, buffer = bufnr }
-
-    local triggerChars = client.server_capabilities.signatureHelpProvider.triggerCharacters
-
-    api.nvim_create_autocmd("TextChangedI", {
-        group = group,
-        buffer = bufnr,
-        callback = function()
-            if check_triggeredChars(triggerChars) then
-                vim.lsp.buf.signature_help({ focus = false })
-            end
-        end,
-    })
+    remap('n', 'K', vim.lsp.buf.hover, opts)
+    remap('n', 'gd', vim.lsp.buf.definition, opts)
+    remap('n', 'gD', vim.lsp.buf.declaration, opts)
+    remap('n', 'gi', vim.lsp.buf.implementation, opts)
+    remap('n', 'go', vim.lsp.buf.type_definition, opts)
+    remap('n', 'gr', vim.lsp.buf.references, opts)
+    remap('n', 'gs', vim.lsp.buf.signature_help, opts)
+    remap('n', '<leader>ra', vim.lsp.buf.rename, opts)
+    remap({ 'n', 'x' }, '<leader>fm', function() vim.lsp.buf.format({ async = true }) end, opts)
+    remap('n', '<leader>a', vim.lsp.buf.code_action, opts)
 end
 
 local function on_attach(client, bufnr)
     load_mappings({ buffer = bufnr })
-
-    if (client.server_capabilities or {}).signatureHelpProvider then
-        setup(client)
-    end
 end
 
 return {
