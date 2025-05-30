382fb2f19024bd172196b76d4af2718aa08ad9f4
IvanB101 <104206321+IvanB101@users.noreply.github.com>
2025-05-30 18:46:05 -0300

    made conform config generic

--- after/ftplugin/python.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set({ 'n', 'x' }, '<leader>fm', function()
-    require("conform").format({ async = true })
-end, { buffer = true })
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,38 @@
--- require("conform").setup({ })
+local opts = {
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		-- rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+}
+
 return {
-    'stevearc/conform.nvim',
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            -- Conform will run multiple formatters sequentially
-            python = { "black" },
-            -- You can customize some of the format options for the filetype (:help conform.format)
-            -- rust = { "rustfmt", lsp_format = "fallback" },
-            -- Conform will run the first available formatter
-            javascript = { "prettierd", "prettier", stop_after_first = true },
-        },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 500,
-            lsp_format = "fallback",
-        },
-    }
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		conform.setup(opts)
+
+		local filetypes = {}
+		for ft in pairs(opts.formatters_by_ft) do
+			filetypes[#filetypes + 1] = ft
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = filetypes,
+			callback = function()
+				vim.keymap.set({ "n", "x" }, "<leader>fm", function()
+					conform.format({ async = true })
+				end, { buffer = true })
+			end,
+		})
+	end,
 }
